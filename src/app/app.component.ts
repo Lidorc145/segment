34264040b2174cf3f73f49aacb5d4160c6100645
Lidor Cohen <LidorC145@Gmail.com>
2020-11-16 11:49:30 +0200

    Application to Segment.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'segment';
+  name = 'Segment';
+  showIt = false;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { OrderTableComponent } from './order-table/order-table.component';
+import { HeaderComponent } from './header/header.component';
+import { RouterModule } from '@angular/router';
+import { HttpService } from './http.service';
+import { HttpClientModule } from '@angular/common/http';
+import { Ng2SmartTableModule } from 'ng2-smart-table';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    Ng2SmartTableModule,
+    NgbModule,
+    RouterModule.forRoot([
+      { path: ``, component: OrderTableComponent },
+      { path: `:filter`, component: OrderTableComponent }
+    ])
   ],
-  providers: [],
+  declarations: [
+    AppComponent,
+    OrderTableComponent,
+    HeaderComponent
+  ],
+  providers: [HttpService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
